5a756f28c8af160f5ebe771ea38a34dd45c36c0e
Tom Naccarato <tcarlnaccarato@gmail.com>
2022-07-14 14:55:43 +0100

    Add phoneNum to attributes

--- PoisedPMS/src/Person.java
+++ PoisedPMS/src/Person.java
@@ -3,16 +3,19 @@ public class Person {
     String role;
     String firstName;
     String surname;
+    String phoneNum;
     String email;
     String address;
 
     // Methods
 
     // Constructor
-    public Person (String role, String firstName, String surname, String email, String address){
+    public Person (String role, String firstName, String surname, String phoneNum, String email,
+                   String address){
         this.role = role;
         this.firstName = firstName;
         this.surname = surname;
+        this.phoneNum = phoneNum;
         this.email = email;
         this.address = address;
     }
