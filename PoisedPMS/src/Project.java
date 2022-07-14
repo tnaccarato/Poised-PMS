ac2220b9b32eabaaa91129d5732d93dde841d272
Tom Naccarato <tcarlnaccarato@gmail.com>
2022-07-14 13:55:05 +0100

    Add Project attributes

--- PoisedPMS/src/Project.java
+++ PoisedPMS/src/Project.java
@@ -1,2 +1,19 @@
+import java.util.Date;
+
 public class Project {
+    // Attributes
+    int projectNum;
+    String projectName;
+    String typeBuilding;
+    String address;
+    String erfNum;
+    double cost;
+    double totalPaid;
+    Date deadline;
+    boolean finalised;
+    Person customer;
+    Person architect;
+    Person contractor;
+    Date completeDate;
+
 }
